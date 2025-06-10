d2e376de336e2398bc437badd36678874e30cb80
Johnny Boy <ddnT5521439SCo@gmail.com>
2025-06-10 18:28:41 +0800

    feat: modify footer and header

--- src/app/(landing)/_components/footer/footer.tsx
+++ src/app/(landing)/_components/footer/footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="w-full border-t bg-background">
+    <footer className="w-full bg-background border-t border-muted-foreground">
       <div className="container mx-auto px-4 md:px-6 2xl:max-w-[1400px] flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <Link href="https://gooda.com.tw" target="_blank" rel="noreferrer">
--- src/app/(landing)/_components/navbar/navbar.tsx
+++ src/app/(landing)/_components/navbar/navbar.tsx
@@ -7,7 +7,7 @@ import { SunIcon } from "lucide-react";
 
 export const Navbar = () => {
   return (
-    <nav className="h-16 bg-background border-b">
+    <nav className="h-16 bg-background shadow-sm shadow-muted-foreground">
       <div className="h-full flex items-center justify-between max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center gap-8">
           <Logo />
