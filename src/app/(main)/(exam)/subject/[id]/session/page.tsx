3a54e5d34cbc6a4f1985bcb92bc017318f714a88
Johnny Boy <ddnT5521439SCo@gmail.com>
2025-06-11 01:31:04 +0800

    refactor: rename landing folder to main folder

---
+++ src/app/(main)/(exam)/subject/[id]/session/page.tsx
@@ -0,0 +1,14 @@
+import SessionTable from "@/app/(landing)/(exam)/_components/session-table";
+
+export default function SessionPage() {
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <div className="max-w-screen-lg w-full px-6 md:max-w-screen-xl sm:px-10">
+        <h2 className="text-2xl  md:text-3xl md:leading-[3.5rem] font-bold tracking-tight sm:max-w-xl sm:text-center sm:mx-auto">
+          測驗列表
+        </h2>
+        <SessionTable />
+      </div>
+    </div>
+  );
+}
