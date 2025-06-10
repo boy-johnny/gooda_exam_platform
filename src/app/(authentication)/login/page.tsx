49207e6d0156a78fc45da42d7d58392d3264fbfa
Johnny Boy <ddnT5521439SCo@gmail.com>
2025-06-10 18:53:36 +0800

    feat: ui-authentication

---
+++ src/app/(authentication)/login/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+import { LoginForm } from "../_components/login-form";
+
+export default function LoginPage() {
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="/" className="flex items-center gap-2 self-center font-medium">
+          <div className="bg-white flex size-10 items-center justify-center rounded-md">
+            <Image src="/logo.png" alt="Gooda" width={30} height={30} />
+          </div>
+          <h2 className="text-center text-xl font-bold leading-loose text-primary">
+            GOODA
+          </h2>
+        </a>
+        <LoginForm />
+      </div>
+    </div>
+  );
+}
