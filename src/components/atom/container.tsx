7385fd4286dfe2d23035e9b2d41aa861f4766232
Johnny Boy <ddnT5521439SCo@gmail.com>
2025-06-10 15:16:57 +0800

    feat: add atom  component

---
+++ src/components/atom/container.tsx
@@ -0,0 +1,22 @@
+export function Container_1() {
+  return (
+    <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+      {/* Content goes here */}
+    </div>
+  );
+}
+export function Container_2() {
+  return (
+    <div className="container mx-auto sm:px-6 lg:px-8">
+      {/* Content goes here */}
+    </div>
+  );
+}
+export function Container_3() {
+  return (
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      {/* We've used 3xl here, but feel free to try other max-widths based on your needs */}
+      <div className="mx-auto max-w-3xl">{/* Content goes here */}</div>
+    </div>
+  );
+}
