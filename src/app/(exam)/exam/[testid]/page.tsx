d2c32c0e22b5c1d400d8ec9760949363d77c01da
Johnny Boy <ddnT5521439SCo@gmail.com>
2025-06-11 10:50:52 +0800

    refactor: refactoring the exam routing

--- src/app/(exam)/exam/[testid]/page.tsx
+++ src/app/(exam)/exam/[testid]/page.tsx
@@ -1,5 +1,5 @@
 // 檔案路徑: app/take-quiz/page.tsx
-import { ExamInterface } from "../../_components/exam-interface";
+import { ExamInterface } from "@/app/(exam)/_components/exam-interface";
 
 export default function TakeQuizPage() {
   return <ExamInterface />;
